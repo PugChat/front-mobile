bfd79379c5cd85de558d4ccd84807811e6c83fb5
dfcastanedat <dfcastanedat@unal.edu.co>
2019-10-21 22:46:52 -0500

    Init

---
+++ android/app/src/main/kotlin/com/example/pugchat_front_mobile/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pugchat_front_mobile
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
